2db878aed19dfb67c3e6e10692545e4f42acac5b
Webbased <fahimhossain892@gmail.com>
2026-07-24 20:21:58 +0600

    Add global configuration for Task Tracker

---
+++ config.js
@@ -0,0 +1,10 @@
+// ==================================
+// Task Tracker
+// Global Configuration
+// ==================================
+
+
+// Google Apps Script Web App URL
+
+const API_URL = 
+"https://script.google.com/macros/s/AKfycbxgxvgpn0S4yfeo25u-DBuK7O9idTgDZbtXUbymdMD_hXN7F2QLKct-MUmtt7DCudB9/exec";
