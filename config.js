8ebf9ff379339590ef58d420ff7f2791c6e0d295
Webbased <fahimhossain892@gmail.com>
2026-07-26 11:13:12 +0600

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@
 // ==================================
 
 
-// Google Apps Script Web App URL
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwbc4OEafv6Llce_x6t-8_PdMJovo2BB-rKhiQiKE0R7gdeAInh3WjIEvzdBMwgU95o/exec"
+
+API_URL:
+"https://script.google.com/macros/s/AKfycbwbc4OEafv6Llce_x6t8-8_PdMJovo2BB-rKhiQiKE0R7gdeAInh3WjIEvzdBMwgU95o/exec"
+
 };
