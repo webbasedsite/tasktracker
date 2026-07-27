40a2b1f2f6e865ed6c8eb2b1bfdbc5431132f6f2
Webbased <fahimhossain892@gmail.com>
2026-07-27 10:44:24 +0600

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
 const CONFIG = {
 
 API_URL:
-"https://script.google.com/macros/s/AKfycbwbc4OEafv6Llce_x6t8-8_PdMJovo2BB-rKhiQiKE0R7gdeAInh3WjIEvzdBMwgU95o/exec"
+"https://script.google.com/macros/s/AKfycbx3i9IQnA__VayvlNKQ-q04cuciqccgrtm3fZLdHqod_k0NHUjlezlLKHttCkHcATuu/exec"
 
 };
